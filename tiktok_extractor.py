adfd4b978e922a7bfdf9d1d3da4010bd00eaf7c3
Zed <Zed>
2025-05-10 04:19:01 +0200

    az

--- tiktok_extractor.py
+++ tiktok_extractor.py
@@ -82,7 +82,7 @@ class TikTokExtractor:
         
         # Configuration standard
         options.add_argument('--no-sandbox')
-        options.add_argument("--headless=new")
+        options.add_argument("--headless")
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1920,1080')
